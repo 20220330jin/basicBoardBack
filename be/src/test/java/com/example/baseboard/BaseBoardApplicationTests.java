2815b1d71374185c9e55460dad180263c41c1591
jin <20220330jin@gmail.com>
2022-04-06 18:26:19 +0900

    [추가] 프로젝트 생성

---
+++ be/src/test/java/com/example/baseboard/BaseBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
